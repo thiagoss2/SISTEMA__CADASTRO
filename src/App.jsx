f2c309ee8081e31fc0f48cc89ce295d306340c00
Thiago Silva <thiagojklb@gmail.com>
2024-07-15 22:42:03 -0300

    criando o inicio do projeto

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Body, ContaineLogin, InputsContainer } from "./styles"
+
+
+function App() {
+  
+
+
+  return (
+    <Body>
+       <ContaineLogin>
+          <h1> LOGIN </h1>
+          <InputsContainer>
+          <input type="text" />
+          <input type="text" />
+          
+          </InputsContainer>
+
+          <button>entrar</button>
+
+
+       </ContaineLogin>
+    </Body>
+  )
+}
+
+
+export default App
---
+++ src/styles.js
@@ -0,0 +1,45 @@
+import styles from "styled-components"
+
+
+export const Body = styles.div`
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;  
+width: 100vw;
+background-color: #000;
+
+
+`;
+
+
+
+
+export const ContaineLogin = styles.div`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+height: 300px;
+width: 300px;
+
+h1{
+    color: #fff;
+    font-size: 30px;
+    margin-bottom: 20px;
+    font-weight: 300;
+    font-family: 'Roboto', sans-serif;
+}
+
+
+
+
+`;
+
+
+export const InputsContainer = styles.div`
+
+`;
+
+
+
